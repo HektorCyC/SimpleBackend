75edd86e6bb283fa5a0339884376295c5318ba21
Camarillo <hcamarillo@novasolutionsystems.com>
2019-09-11 22:25:15 -0500

    fix(cors)

--- app.js
+++ app.js
@@ -13,15 +13,7 @@ var project_routes = require('./routes/routes');
 // Middleware
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
-
-app.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next();
- });
- 
- 
-
+  
 // Routing
 app.use('/', project_routes);
 
